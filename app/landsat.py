4f553fcac005057e85d7c5ea929936f418e6e73e
vincentsarago <vincent.sarago@mapbox.com>
2018-02-05 14:26:39 -0500

    adapt to rio-tiler 1.0a3

--- app/landsat.py
+++ app/landsat.py
@@ -2,46 +2,28 @@
 
 import re
 import json
-from functools import reduce
 
 import numpy as np
-import numexpr as ne
 
 from rio_tiler import landsat8
-from rio_tiler.utils import array_to_img, linear_rescale, get_colormap
+from rio_tiler.utils import array_to_img, linear_rescale, get_colormap, expression
 
 from aws_sat_api.search import landsat as landsat_search
 
 from lambda_proxy.proxy import API
 
-LANDSAT_APP = API(app_name="landsat-tiler")
-
-RATIOS = {
-    'ndvi': {
-        'eq': '(b5 - b4) / (b5 + b4)',
-        'rg': [-1, 1]},
-    'ndsi': {
-        'eq': '(b2 - b5) / (b2 + b5)',
-        'rg': [-1, 1]},
-    'ndwi': {
-        'eq': '(b5 - b6) / (b5 + b6)',
-        'rg': [-1, 1]},
-    'ac-index': {
-        'eq': '(b1 - b2) / (b1 + b2)',
-        'rg': [-1, 1]}}
+APP = API(app_name="landsat-tiler")
 
 
 class LandsatTilerError(Exception):
     """Base exception class"""
 
 
-@LANDSAT_APP.LANDSAT_APP('/landsat/search', methods=['GET'], cors=True)
+@APP.route('/landsat/search', methods=['GET'], cors=True)
 def search():
+    """Handle search requests
     """
-    Handle search requests
-    """
-
-    query_args = LANDSAT_APP.current_request.query_params
+    query_args = APP.current_request.query_params
     query_args = query_args if isinstance(query_args, dict) else {}
 
     path = query_args['path']
@@ -57,21 +39,19 @@ def search():
     return ('OK', 'application/json', json.dumps(info))
 
 
-@LANDSAT_APP.route('/landsat/bounds/<scene>', methods=['GET'], cors=True)
-def landsat_bounds(scene):
-    """
-    Handle bounds requests
+@APP.route('/landsat/bounds/<scene>', methods=['GET'], cors=True)
+def bounds(scene):
+    """Handle bounds requests
     """
     info = landsat8.bounds(scene)
     return ('OK', 'application/json', json.dumps(info))
 
 
-@LANDSAT_APP.route('/landsat/metadata/<scene>', methods=['GET'], cors=True)
-def landsat_metadata(scene):
+@APP.route('/landsat/metadata/<scene>', methods=['GET'], cors=True)
+def metadata(scene):
+    """Handle metadata requests
     """
-    Handle metadata requests
-    """
-    query_args = LANDSAT_APP.current_request.query_params
+    query_args = APP.current_request.query_params
     query_args = query_args if isinstance(query_args, dict) else {}
 
     pmin = query_args.get('pmin', 2)
@@ -84,12 +64,11 @@ def landsat_metadata(scene):
     return ('OK', 'application/json', json.dumps(info))
 
 
-@LANDSAT_APP.route('/landsat/tiles/<scene>/<int:z>/<int:x>/<int:y>.<ext>', methods=['GET'], cors=True)
-def landsat_tile(scene, tile_z, tile_x, tile_y, tileformat):
+@APP.route('/landsat/tiles/<scene>/<int:z>/<int:x>/<int:y>.<ext>', methods=['GET'], cors=True)
+def tile(scene, tile_z, tile_x, tile_y, tileformat):
+    """Handle tile requests
     """
-    Handle tile requests
-    """
-    query_args = LANDSAT_APP.current_request.query_params
+    query_args = APP.current_request.query_params
     query_args = query_args if isinstance(query_args, dict) else {}
 
     bands = query_args.get('rgb', '4,3,2')
@@ -106,65 +85,50 @@ def landsat_tile(scene, tile_z, tile_x, tile_y, tileformat):
     tilesize = int(tilesize) if isinstance(tilesize, str) else tilesize
 
     pan = True if query_args.get('pan') else False
+    tile, mask = landsat8.tile(scene, tile_x, tile_y, tile_z, bands, pan=pan, tilesize=tilesize)
 
-    tile = landsat8.tile(scene, tile_x, tile_y, tile_z, bands, pan=pan, tilesize=tilesize)
     rtile = np.zeros((len(bands), tilesize, tilesize), dtype=np.uint8)
     for bdx in range(len(bands)):
-        rtile[bdx] = np.where(
-            tile[bdx] > 0,
-            linear_rescale(tile[bdx], in_range=histoCut[bdx], out_range=[1, 255]), 0)
+        rtile[bdx] = np.where(mask, linear_rescale(tile[bdx], in_range=histoCut[bdx], out_range=[0, 255]), 0)
 
-    tile = array_to_img(rtile, tileformat)
+    tile = array_to_img(rtile, tileformat, mask=mask)
+
     if tileformat == 'jpg':
         tileformat = 'jpeg'
 
     return ('OK', f'image/{tileformat}', tile)
 
 
-@LANDSAT_APP.route('/landsat/processing/<scene>/<int:z>/<int:x>/<int:y>.<ext>', methods=['GET'], cors=True)
-def landsat_ratio(scene, tile_z, tile_x, tile_y, tileformat):
+@APP.route('/landsat/processing/<scene>/<int:z>/<int:x>/<int:y>.<ext>', methods=['GET'], cors=True)
+def ratio(scene, tile_z, tile_x, tile_y, tileformat):
+    """Handle processing requests
     """
-    Handle processing requests
-    """
-    query_args = LANDSAT_APP.current_request.query_params
+    query_args = APP.current_request.query_params
     query_args = query_args if isinstance(query_args, dict) else {}
 
-    ratio_value = query_args.get('ratio', 'ndvi')
+    ratio_value = query_args['ratio']
+    APP.log.debug(f'{ratio_value}')
 
-    if ratio_value not in RATIOS.keys():
-        raise LandsatTilerError('Invalid ratio: {}'.format(ratio_value))
-
-    equation = RATIOS[ratio_value]['eq']
-    band_names = list(set(re.findall('b[0-9]{1,2}', equation)))
-    bands = tuple(map(lambda x: x.strip('b'), band_names))
+    range_value = query_args.get('range', [-1, 1])
 
     tilesize = query_args.get('tile', 256)
     tilesize = int(tilesize) if isinstance(tilesize, str) else tilesize
 
-    tile = landsat8.tile(scene, tile_x, tile_y, tile_z, bands, tilesize=tilesize)
-    for bdx, b in enumerate(band_names):
-        globals()[b] = tile[bdx]
+    tile, mask = expression(scene, tile_x, tile_y, tile_z, ratio_value, tilesize=tilesize)
+    if len(tile.shape) == 2:
+        tile = np.expand_dims(tile, axis=0)
 
-    tile = np.where(
-        reduce(lambda x, y: x*y, [globals()[i] for i in band_names]) > 0,
-        np.nan_to_num(ne.evaluate(equation)),
-        -9999)
+    rtile = np.where(mask, linear_rescale(tile, in_range=range_value, out_range=[0, 255]), 0).astype(np.uint8)
+    tile = array_to_img(rtile, tileformat, color_map=get_colormap(name='cfastie'), mask=mask)
 
-    range_val = equation = RATIOS[ratio_value]['rg']
-    rtile = np.where(
-            tile != -9999,
-            linear_rescale(tile, in_range=range_val, out_range=[1, 255]), 0).astype(np.uint8)
-
-    tile = array_to_img(rtile, tileformat, color_map=get_colormap(name='cfastie'))
     if tileformat == 'jpg':
         tileformat = 'jpeg'
 
     return ('OK', f'image/{tileformat}', tile)
 
 
-@LANDSAT_APP.route('/favicon.ico', methods=['GET'], cors=True)
+@APP.route('/favicon.ico', methods=['GET'], cors=True)
 def favicon():
-    """
-    favicon
+    """favicon
     """
     return('NOK', 'text/plain', '')
